c6ed74d656989e6634a9ecb9d881bb58c43b54f9
Ian Gwillim <igwillim@gmail.com>
2021-10-11 00:12:52 -0400

    closes #1

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,26 +2,37 @@
 let elemetnIsVisible = false;
 let trackArtistElement = document.querySelector(".trackArtist");
 let trackTitleElement = document.querySelector(".trackTitle");
+let musicHorizontalItem = document.querySelector("music-horizontal-item");
 let currentSongName = null;
 let currentArtist = null;
 let songData = {songName: "", artistName: ""};
+
+console.log("\nAmazon Music Extension -- Content Sctript Loaded\n");
 
 // Create port for passing track info to background.js
 let port = chrome.runtime.connect({ name: "amazonMusic" });
 
 // If the music hasn't been started then these elements are not visible
 function areElementsVisible() {
-  if (trackArtistElement !== null && trackTitleElement !== null) {
+
+  if (musicHorizontalItem !== null) {
     elemetnIsVisible = true;
+
+    console.log(`Music Horizontal Item Found...`);
+    
+    console.log(musicHorizontalItem.getAttribute("primary-text"));
+    console.log(musicHorizontalItem.getAttribute("secondary-text"));
   }
+
 }
 
 // Check that music has started playing.
 // Once music is playing the elements that contain the song and artist values will be available.
 let checkIfElementsAreVisible = setInterval(() => {
   // Try and set the element
-  trackArtistElement = document.querySelector(".trackArtist");
-  trackTitleElement = document.querySelector(".trackTitle");
+  musicHorizontalItem = document.querySelector("music-horizontal-item");
+
+  // music-horizontal-item
 
   // Are the elements visible yet (is music playing).
   areElementsVisible();
@@ -44,16 +55,19 @@ let checkIfElementsAreVisible = setInterval(() => {
 // Update the song and artist if it has changed.
 function handleTrackData() {
   // Set the most recent track info
-  songData.artistName = trackArtistElement.children[0].innerText;
-  songData.songName = trackTitleElement.children[0].innerText;
+  // songData.artistName = trackArtistElement.children[0].innerText;
+  // songData.songName = trackTitleElement.children[0].innerText;
+
+  songData.songName = musicHorizontalItem.getAttribute("primary-text");
+  songData.artistName = musicHorizontalItem.getAttribute("secondary-text");
 
   // When music starts playing set the current song and artisit
   if (currentSongName === null || currentArtist === null) {
     // Send to background.js now that we have track info
     port.postMessage(JSON.stringify(songData));
     
-    currentSongName = trackTitleElement.children[0].innerText;
-    currentArtist = trackArtistElement.children[0].innerText;
+    currentSongName = musicHorizontalItem.getAttribute("primary-text");
+    currentArtist = musicHorizontalItem.getAttribute("secondary-text");
   }
 
   // Only update current track info if it has changed
@@ -61,8 +75,10 @@ function handleTrackData() {
     // Send the new track info to background.js.
     port.postMessage(JSON.stringify(songData));
 
-    currentSongName = trackTitleElement.children[0].innerText;
-    currentArtist = trackArtistElement.children[0].innerText;
+    currentSongName = musicHorizontalItem.getAttribute("primary-text");
+    currentArtist = musicHorizontalItem.getAttribute("secondary-text");
+
+    console.log("New Song...");
   }
 
 }
